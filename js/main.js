e32f91c4b5a85f7fecf23dfece8c215f5c0f746e
crossm2 <crossm2@seattleu.edu>
2016-02-19 17:43:11 -0500

    finished assignment and stretch goals

--- js/main.js
+++ js/main.js
@@ -5,4 +5,45 @@
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
 
-// Refer to the `index.html` file for the validation rules that must be enforced.
+//document ready function
+$( document ).ready(function(){
+//bring focus to the first field after the page loads
+	$("#your-name").focus();
+	
+// Form validation
+    $('#order-form').validate({
+        submitHandler: function(form) {
+            form.submit();
+        },
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128
+            },
+            "your-state": {
+                required: true,
+                maxlength: 2
+            },
+            "your-zip": {
+                required: true,
+                maxlength: 5,
+                digits: true
+            },
+            "card-holder-name": {
+                required: true,
+                maxlength: 128
+            },
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+            "cvv": {
+                required: true,
+                maxlength: 3,
+                digits: true
+            }
+        }
+    });
+    // add in  Tooltips
+    $('label span.glyphicon').tooltip();
+});
